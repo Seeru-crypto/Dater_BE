f351fa99bba52c7e9eeaa0114ca97b95ac8495fc
Fred <iblueman260@gmail.com>
2021-10-24 12:19:03 +0300

    gradle ini

---
+++ src/main/java/com/example/Dater_BE/DaterBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.Dater_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaterBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaterBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Dater_BE/DaterBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Dater_BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaterBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
