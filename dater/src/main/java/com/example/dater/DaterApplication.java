c20086e19924d209af7203921089b43f53d478a4
Fred <iblueman260@gmail.com>
2021-11-03 18:30:20 +0200

    new project ini

---
+++ dater/src/main/java/com/example/dater/DaterApplication.java
@@ -0,0 +1,13 @@
+package com.example.dater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaterApplication.class, args);
+	}
+
+}
--- dater/src/test/java/com/example/dater/DaterApplicationTests.java
+++ dater/src/test/java/com/example/dater/DaterApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.Dater;
+package com.example.dater;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
