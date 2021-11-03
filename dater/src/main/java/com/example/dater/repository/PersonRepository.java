1960550bf11d4d45848d82d6c790af6ab815f674
Fred <iblueman260@gmail.com>
2021-11-03 21:14:30 +0200

    mid-way save

---
+++ dater/src/main/java/com/example/dater/repository/PersonRepository.java
@@ -0,0 +1,15 @@
+package com.example.dater.repository;
+
+import java.util.List;
+
+import com.example.dater.model.Person;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(collectionResourceRel = "people", path = "people")
+public interface PersonRepository extends MongoRepository<Person, String> {
+
+  List<Person> findByLastName(@Param("name") String name);
+
+}
