e6f2e09f2ed7bd0ea24fa4824d227213933f9b70
Seeru <iBlueman260@gmail.com>
2021-11-09 22:24:46 +0200

    fix cors error

--- src/main/java/com/example/Dater_BE/repository/EventRepository.java
+++ src/main/java/com/example/Dater_BE/repository/EventRepository.java
@@ -5,8 +5,11 @@ import java.util.List;
 import com.example.Dater_BE.model.Event;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin(origins = "http://localhost:4001")
 @RepositoryRestResource(collectionResourceRel = "event", path = "event")
+
 public interface EventRepository extends MongoRepository<Event, String> {
 
     List<Event> findByEventName(String eventName);
