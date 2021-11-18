df778b6113b458651df1ca15a87b8636b9b105b8
Seeru <iBlueman260@gmail.com>
2021-11-19 00:10:56 +0200

    delete broken script, clean code

--- src/main/java/com/example/dater/service/SendMailServiceImpl.java
+++ src/main/java/com/example/dater/service/SendMailServiceImpl.java
@@ -1,6 +1,5 @@
 package com.example.dater.service;
 
-import org.springframework.context.annotation.Profile;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
