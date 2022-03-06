f12e89184085e68de6eae5144c2c3273eff4f245
Fred-Andree Oja <fred-andree.oja@netgroup.com>
2022-03-06 14:21:53 +0200

    Patch/1.3 refactor EventDateChecker, add testCases
    Took 56 minutes

--- src/main/java/com/example/dater/schedulingtasks/EventDateChecker.java
+++ src/main/java/com/example/dater/schedulingtasks/EventDateChecker.java
@@ -2,7 +2,6 @@ package com.example.dater.schedulingtasks;
 
 import com.example.dater.model.Event;
 import com.example.dater.repository.EventRepository;
-import com.example.dater.service.HelperFunctions;
 import com.example.dater.service.SendMailService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
@@ -13,6 +12,7 @@ import javax.mail.MessagingException;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
@@ -33,38 +33,50 @@ public class EventDateChecker {
         this.sendMailService = sendMailService;
     }
 
+    protected Boolean dateVerification(Event event, Instant currentDate){
+        if (Boolean.FALSE.equals(event.getReminder())) {
+            return false;
+        }
+        Instant reminderDate = event.getDateNextReminder().truncatedTo(ChronoUnit.DAYS);
+
+        if (Boolean.TRUE.equals(event.getAccountForYear()) && currentDate.equals(reminderDate)){
+            return true;
+        }
+        else if (Boolean.FALSE.equals(event.getAccountForYear())){
+            ZoneId z = ZoneId.systemDefault();
+            ZonedDateTime currentDateTime = ZonedDateTime.ofInstant(currentDate, z);
+            ZonedDateTime reminderDateTime = ZonedDateTime.ofInstant(reminderDate, z);
+
+            Boolean areDaysEqual = (currentDateTime.getDayOfMonth() == reminderDateTime.getDayOfMonth());
+            Boolean areMonthsEqual = (currentDateTime.getMonthValue() == reminderDateTime.getMonthValue());
+
+            return areDaysEqual && areMonthsEqual;
+        }
+        return false;
+    }
+
     public void checkEventDates(String initiatedBy) throws MessagingException {
         eventList = eventRepository.findAll();
         List<Event> eventsToSendOut = new ArrayList<>();
+        Instant currentDate = Instant.now().truncatedTo(ChronoUnit.DAYS);
+        DateTimeFormatter myFormatter = DateTimeFormatter.ofPattern("dd.MMM.yy");
+
         for (Event event : eventList) {
-            if (Boolean.FALSE.equals(event.getReminder())) {
-                continue;
-            }
-
-            Instant currentDate = Instant.now().truncatedTo(ChronoUnit.DAYS);
-            Instant reminderDate = event.getDateNextReminder().truncatedTo(ChronoUnit.DAYS);
-
-            if (currentDate.equals(reminderDate)){
+            if (Boolean.TRUE.equals(dateVerification(event, currentDate))) {
+                LocalDate eventDate =  event.getDate().atZone(ZoneId.systemDefault()).toLocalDate();
+                event.setMailDisplayDate(myFormatter.format(eventDate));
                 eventsToSendOut.add(event);
-            };
-
-            // ToDo fix account for year logic
+            }
         }
 
-        log.info("******************");
         if (eventsToSendOut.isEmpty()) {
             log.info("No events to send out");
             return;
         }
 
-        for (int a = 0; a < eventsToSendOut.size(); a++) {
-            Event event = eventsToSendOut.get(a);
-            LocalDate eventDate =  event.getDate().atZone(ZoneId.systemDefault()).toLocalDate();
-            DateTimeFormatter myFormatter = DateTimeFormatter.ofPattern("dd.MMM.yy");
-            event.setMailDisplayDate(myFormatter.format(eventDate));
-            eventsToSendOut.set(a, event);
-        }
+        log.info("Events to be sent:");
+        for (Event event: eventsToSendOut) log.info(event.getName());
+
         sendMailService.sendMimeMailList(eventsToSendOut, initiatedBy);
     }
-
 }
---
+++ src/test/java/com/example/dater/schedulingtasks/EventDateCheckerTest.java
@@ -0,0 +1,31 @@
+package com.example.dater.schedulingtasks;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EventDateCheckerTest {
+
+    private EventDateChecker eventDateChecker;
+
+    // ToDO create tests for date verification
+    @Test
+    void dateVerification() {
+    }
+
+    @Test
+    void shouldReturnFalse_whenReminderIsFalse() {
+    }
+
+    @Test
+    void shouldReturnFalse_whenReminderIsTrueButDatesDontMatch() {
+    }
+
+    @Test
+    void shouldReturnTrue_whenDatesMatchAndAccountForYearIsTrue() {
+    }
+
+    @Test
+    void shouldReturnTrue_whenDaysAndMonthsMatchAndAccountForYearIsFalse() {
+    }
+}
