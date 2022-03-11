19806978b2af0924c84b4369a99c50f415ab8af1
Fred-Andree Oja <fred-andree.oja@netgroup.com>
2022-03-11 18:10:34 +0200

    Patch/1.3.5 fix remove email regex from settingDTO
    Took 15 minutes

--- src/main/java/com/example/dater/model/SettingsDTO.java
+++ src/main/java/com/example/dater/model/SettingsDTO.java
@@ -4,12 +4,10 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.time.Instant;
 
-import static com.example.dater.model.Settings.EMAIL_REGEX;
 import static com.example.dater.model.Settings.MAX_EMAIL_LEN;
 
 @Getter
@@ -19,7 +17,6 @@ public class SettingsDTO {
     @NotNull
     private Boolean isEmailActive;
     @Size(max = MAX_EMAIL_LEN, message = "Email is too long")
-    @Email(message = "Email is not valid", regexp = EMAIL_REGEX)
     private String emailAddress;
     private Instant dateUpdated;
 }
